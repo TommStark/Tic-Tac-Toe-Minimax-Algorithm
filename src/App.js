ea54386c3249ab6fd9dd56b1c8b3217fbf8837b3
tommstark <posik.tomas@gmail.com>
2019-08-12 21:02:42 -0300

    add IA

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Tile from './components/Tile'
 
 import './App.css';
 
+let fc = 0;
+
 class App extends Component {
   constructor() {
     super();
@@ -15,6 +17,9 @@ class App extends Component {
       winner: null,
       moves: 0,
       lineWin: null,
+      huPlayer: 'X',
+      aiPlayer: 'O',
+      prevTurn: 'O'
     }
     this.updatedBoard = this.updatedBoard.bind(this);
     this.resetBoard = this.resetBoard.bind(this);
@@ -27,12 +32,120 @@ class App extends Component {
       winner: null,
       moves: 0,
       lineWin: null,
+      huPlayer: 'X',
+      aiPlayer: 'O',
+      prevTurn: 'O'
     })
   }
 
+  copyBoard(board) {
+    return board.slice(0);
+  }
+
+  tie(board) {
+    let moves = board.join('').replace(/ /g, '');
+    if (moves.length === 9) {
+      return true;
+    }
+    return false;
+  }
+
+  validMove(move, player, board) {
+    debugger
+
+    let newBoard = this.copyBoard(board);
+
+    if (newBoard[move] === null) {
+      newBoard[move] = player;
+      return newBoard;
+    } else{
+      return null;
+    }
+  }
+
+  minScore(board) {
+    if (!!this.calculateWinner(board, this.state.huPlayer)[0]) {
+      return 10;
+    }
+    else if (!!this.calculateWinner(board, this.state.aiPlayer)[0]) {
+      return -10;
+    }
+    else if (this.tie(board)) {
+      return { score: 0 };
+    } else {
+      let bestMoveValue = 100;
+      let move = 0;
+      for (let i = 0; i < board.length; i++) {
+        let newBoard = this.validMove(i,  this.state.aiPlayer, board);
+        if (newBoard) {
+          let predictedMoveValue = this.maxScore(newBoard);
+          if (predictedMoveValue < bestMoveValue) {
+            bestMoveValue = predictedMoveValue;
+            move = i;
+          }
+        }
+      }
+      //console.log("Best Move Value(minScore):", bestMoveValue);
+      return bestMoveValue;
+    }
+  }
+
+  maxScore(board) {
+    if (!!this.calculateWinner(board, this.state.huPlayer)[0]) {
+      return 10;
+    }
+    else if (!!this.calculateWinner(board, this.state.aiPlayer)[0]) {
+      return -10;
+    }
+    else if (this.tie(board)) {
+      return { score: 0 };
+    } else {
+      let bestMoveValue = -100;
+      let move = 0;
+      for (let i = 0; i < board.length; i++) {
+        let newBoard = this.validMove(i, this.state.huPlayer, board);
+        if (newBoard) {
+          let predictedMoveValue = this.minScore(newBoard);
+          if (predictedMoveValue > bestMoveValue) {
+            bestMoveValue = predictedMoveValue;
+            move = i;
+          }
+        }
+      }
+      return bestMoveValue;
+    }
+  }
+
+  //minimax IA function
+  minimax(board) {
+    let bestMoveScore = 100;
+    let move = null;
+
+    if (!!this.calculateWinner(board, this.state.huPlayer)[0] || !!this.calculateWinner(board, this.state.aiPlayer)[0] || this.tie(board)) {
+      return null
+    }
+
+    for (let i = 0; i < board.length; i++) {
+
+      let newBoard = this.validMove(i, this.state.aiPlayer, board);
+
+      //If validMove returned a valid game board
+      if (newBoard) {
+        let moveScore = this.maxScore(newBoard);
+        if (moveScore < bestMoveScore) {
+          bestMoveScore = moveScore;
+          move = i;
+        }
+      }
+    }
+    return move;
+  }
+
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.state.turn !== prevState.turn) {
-      const winArr = this.calculateWinner(this.state.gameBoard);
+    console.log(` prev turn : ${this.state.prevTurn}`)
+    console.log(` turn : ${this.state.turn}`)
+    if (this.state.turn !== prevState.turn && this.state.gameBoard !== prevState.gameBoard) {
+      const winArr = this.calculateWinner(this.state.gameBoard, this.state.prevTurn);
       if (winArr[0] !== null) {
         this.setState({
           winner: winArr[0],
@@ -44,10 +157,23 @@ class App extends Component {
           winner: 0
         })
       }
+      if(this.state.turn !== prevState.turn && this.state.turn === "O"){
+        const squares = this.state.gameBoard.slice();
+        let ia = this.minimax(squares);
+        squares[ia] = this.state.turn;
+        const moves = this.state.moves;
+        this.setState({
+          gameBoard: squares,
+          moves: moves + 1,
+          turn:'X',
+          prevTurn: (this.state.prevTurn === 'O') ? 'X' : 'O'
+        });
+        console.log(squares)
+      }
     }
   }
 
-  calculateWinner(squares) {
+  calculateWinner(squares, player) {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -60,7 +186,7 @@ class App extends Component {
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (squares[a] === player && squares[b] === player && squares[c] === player) {
         return [squares[a], lines[i]];
       }
     }
@@ -69,31 +195,32 @@ class App extends Component {
 
   handleClick(i) {
     const squares = this.state.gameBoard.slice();
-    squares[i] = this.state.turn;
+    // squares[i] = this.state.turn;
+    squares[i] = "X";
     const moves = this.state.moves;
     this.setState({
       gameBoard: squares,
       moves: moves + 1
     });
-
   }
 
   updatedBoard(loc) {
     if (this.state.gameBoard[loc] === 'X' || this.state.gameBoard[loc] === 'O' || this.state.winner) {
-      console.log('invalid move')
       return;
     } else {
       this.handleClick(loc)
       this.setState({
-        turn: (this.state.turn === 'X') ? 'O' : 'X'
+        // turn: (this.state.turn === 'X') ? 'O' : 'X',
+        turn:'O',
+        prevTurn: (this.state.prevTurn === 'O') ? 'X' : 'O'
       })
     }
   }
 
-  paint(index){
-    if(this.state.lineWin !== null){
+  paint(index) {
+    if (this.state.lineWin !== null) {
       let exist = this.state.lineWin.filter(i => i === index);
-      console.log(exist);
+      // console.log(exist);
       return exist.length === 1;
     }
   }
